3c28c31d63d2443c22f927b1db57ea8d6659380b
brainzcode <98150590+brainzcode@users.noreply.github.com>
2022-05-04 18:26:13 +0100

    Initial Commit

---
+++ iFRUIT/iFRUITApp.swift
@@ -0,0 +1,17 @@
+//
+//  iFRUITApp.swift
+//  iFRUIT
+//
+//  Created by SOG on 5/4/22.
+//
+
+import SwiftUI
+
+@main
+struct iFRUITApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
